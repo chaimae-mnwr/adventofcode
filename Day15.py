b6ce492980b91f43fbc85d1e74b1397efd0a399a
Chaimae.MENOUAR <chaimae.menouar@grenoble-inp.org>
2021-01-04 11:39:55 +0100

    day 15

---
+++ Day15.py
@@ -0,0 +1,7 @@
+fichier = open('/home/menouar/Documents/dir5/ship.txt', 'r')
+directions = []
+
+for line in fichier:
+    if line != "\n":
+        line_split = (line.rstrip('\n'))
+        directions.append([line_split[0],int(line_split[1:])])
