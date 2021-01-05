e5a2227201379416bc5662c44ab3d055a1f24164
Chaimae.MENOUAR <chaimae.menouar@grenoble-inp.org>
2021-01-05 23:54:39 +0100

    Day 14 Test 1

--- Day14.py
+++ Day14.py
@@ -1,6 +1,6 @@
 #Test 1:
 fichier = open('/home/menouar/Documents/dir5/bits.txt', 'r')
-
+adresses = {}
 masks = []
 vals = []
 i = -1
@@ -14,10 +14,21 @@ for line in fichier:
         a = line_split[0].index("[")
         b = line_split[0].index("]")
         adress = line_split[0][a+1:b]
-        print(line_split)
-        vals[i].append((int(adress), int(line_split[2])))
-print(masks)
-print(vals)
 
-pass
-pass
+        vals[i].append((int(adress), int(line_split[2])))
+
+
+for i in range(0, len(masks)):
+    for value in vals[i]:
+        adress = value[0]
+        binary = "{:036b}".format(value[1])
+        result = binary
+        result = list(result)
+        print(result)
+        for j in range(0, len(masks[i])):
+            if masks[i][j] == '1' or masks[i][j] == '0':
+                result[j] = masks[i][j]
+        result = ''.join(result)
+        adresses[adress] = int(result, 2)
+
+print(sum(adresses.values()))
